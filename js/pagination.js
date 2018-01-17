5d013c450502b10df9d1b1e2fc831eae803327a8
Lut <wakawaka2610@gmail.com>
2018-01-17 21:15:55 +0700

    fix3

--- js/pagination.js
+++ js/pagination.js
@@ -2,17 +2,25 @@
 this javascript is only to change the "actpage" attribut on the .cdp div
 */
 
-window.onload = function(){
+function onClickCdpItem(event) {
   var paginationPage = parseInt($('.cdp').attr('actpage'), 10);
-  $('.cdp_i').on('click', function(){
-    var go = $(this).attr('href').replace('#!', '');
-    if (go === '+1') {
-      paginationPage++;
-    } else if (go === '-1') {
-      paginationPage--;
-    }else{
-      paginationPage = parseInt(go, 10);
-    }
-    $('.cdp').attr('actpage', paginationPage);
+  var go = $(event.target).attr('href').replace('#!', '');
+  
+  if (go === '+1') {
+    paginationPage++;
+
+  } else if (go === '-1') {
+    paginationPage--;
+  }else{
+    paginationPage = parseInt(go, 10);
+  }
+
+  $('.cdp').attr('actpage', paginationPage);
+
+ $(".image-container[class*='page']").each(function() {
+    var visible = $(this).attr("class").indexOf("page" + paginationPage) > -1 ? true : false;
+    if(visible)
+      $(this).css("display", "");
+    else $(this).css("display", "none");
   });
-};
+}
